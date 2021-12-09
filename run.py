5d8c91bc5b67bee56f6aa1f6d1890bb5772b3416
alfa23 <paw235@gmail.com>
2021-12-09 15:51:23 +0000

    add: Set up game variables and main loop, add user introduction/instructions

--- run.py
+++ run.py
@@ -1,19 +1,65 @@
 """
-Import random function and declare global symbol variables
-for the game and output user info and instructions.
+Import random function and declare global symbol
+variables for the game.
 """
 
 import random
 symbols = ['€', '£', '$']
-taxman = ['@']
+# taxman = ['@']
 
+# """
+# Set wallet variable for new game and establish main game
+# infinite while loop to ensure the program runs continuously
+# after each successful game/execution.
+# Output user info and instructions
+# """
+
+# while True:
+
+wallet = 100
+
+print('The Python ONE ARM BANDIT\n')
 print()
+print('(Orig. US,gambling) A gaming machine\n')
+print('having a long arm-like handle at one\n')
+print('side that a player pulls down to make\n')
+print('reels spin; the player wins money or\n')
+print('tokens when certain combinations of\n')
+print('symbols line up on these reels.\n')
+print()
+print('AKA: Fruit machine, slot machine\n')
+print()
+print('• https://en.wiktionary.org/wiki/\n')
+print('  one-armed_bandit\n')
+print()
+print('****************************************\n')
+print('MATCH THREE SYMBOLS:\n')
+print('£££ / $$$ / €€€\n')
+print('WIN wager x 3!!!\n')
+print()
+print('MATCH TWO SYMBOLS:\n')
+print('££- / $$- / €€- and -££ / -$$ / -€€\n')
+print('WIN wager x 2!!\n')
+print()
+username = input('What is your name? \n')
+print()
+print(f'Hey {username}, you have {wallet} credits in your wallet!\n')
+print('Minimum wager is 1 credit.\n')
 
-"""
-Set wallet variable for new game and establish main game
-infinite while loop to ensure the program runs continuously
-after each successful game execution.
-"""
-
-while True:
-    wallet = 100
+while wallet > 0:
+    try:
+        wager = int(input('How much do you want to wager? \n'))
+    except:
+        print('Please wager a whole number of credits.\n')
+        continue
+    if wager > wallet:
+        print(f'Sorry {username}, insufficient credits!\n')
+    else:
+        wallet -= wager
+        reel_1 = random.choice(symbols)
+        reel_2 = random.choice(symbols)
+        reel_3 = random.choice(symbols)
+    
+    print(f'     |{random.choice(symbols)}|{random.choice(symbols)}|{random.choice(symbols)}|')
+    print(f' WIN•|{reel_1}|{reel_2}|{reel_3}|•LINE')
+    print(f'     |{random.choice(symbols)}|{random.choice(symbols)}|{random.choice(symbols)}|')
