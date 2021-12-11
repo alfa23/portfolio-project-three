2cfbcd01e3cbfa8d32d96dce191af7409172dce1
alfa23 <paw235@gmail.com>
2021-12-11 11:10:36 +0000

    Restyle code to fit width

--- run.py
+++ run.py
@@ -19,49 +19,62 @@ while True:
     wallet = 100
 
     print()
-    print('############################• T H E   P Y T H O N •############################')
-    print('#                                                                             #')
-    print('#   #     ##   ###   #   #  ####  ###     ###    ##   #  #  ###   ###  #####  #')
-    print('#  ##    #  #  #  #  ## ##  #     #  #    #  #  #  #  ## #  #  #   #     #    #')
-    print('#   # ## ####  ###   # # #  ###   #  #    ###   ####  # ##  #  #   #     #    #')
-    print('#   #    #  #  #  #  #   #  #     #  #    #  #  #  #  #  #  #  #   #     #    #')
-    print('#  ###   #  #  #  #  #   #  ####  ###     ###   #  #  #  #  ###   ###    #    #')
-    print('#                                                                             #')
-    print('###############################################################################')
+    print(
+          '    #########• T H E   P Y T H O N •#########\n'
+          '    #                                       #\n'
+          '    #   #     ##   ###   #   #  ####  ###   #\n'
+          '    #  ##    #  #  #  #  ## ##  #     #  #  #\n'
+          '    #   # ## ####  ###   # # #  ###   #  #  #\n'
+          '    #   #    #  #  #  #  #   #  #     #  #  #\n'
+          '    #  ###   #  #  #  #  #   #  ####  ###   #\n'
+          '    #                                       #\n'
+          '    #  ###    ##   #   #  ###   ###  #####  #\n'
+          '    #  #  #  #  #  ##  #  #  #   #     #    #\n'
+          '    #  ###   ####  # # #  #  #   #     #    #\n'
+          '    #  #  #  #  #  #  ##  #  #   #     #    #\n'
+          '    #  ###   #  #  #   #  ###   ###    #    #\n'
+          '    #                                       #\n'
+          '    #########################################\n')
     print()
     print('  https://en.wiktionary.org/wiki/one-armed_bandit:')
     print()
-    print('  From one-armed (“having only one arm”) + bandit (“one who robs others in')
-    print('  a lawless area, especially as part of a group; one who cheats others”),')
-    print('  referring to the fact that the machine is operated by a single handle')
-    print('  and “steals” money from losing players.')
+    print('  From one-armed (“having only one arm”) + bandit (“one\n'
+          '  who robs others in a lawless area, especially as part\n'
+          '  of a group; one who cheats others”),referring to the\n'
+          '  fact that the machine is operated by a single handle\n'
+          '  and “steals” money from losing players.\n')
     print()
-    print('  (Orig. US, gambling) A gaming machine having a long arm-like handle at one')
-    print('  side that a player pulls down to make reels spin; the player wins money or')
-    print('  tokens when certain combinations of symbols line up on these reels.')
+    print('  (Orig. US, gambling) A gaming machine having a long\n'
+          '  arm-like handle at one side that a player pulls down to\n'
+          '  make reels spin; the player wins money or tokens when\n'
+          '  certain combinations of symbols line up on these reels.\n')
     print()
-    print('  AKA: Fruit machine, poker machine/pokie/pokie machine, slot machine')
+    print('  AKA: Fruit machine, poker machine, slot machine')
     print()
-    print('•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••')
-    print('  MATCH THREE SYMBOLS:    •   MATCH TWO SYMBOLS:')
-    print('  €€€ / £££ / $$$ / ¥¥¥   •   €€- / ££- / $$- / ¥¥- or -€€ / -££ / -$$ / -¥¥')
-    print('  WIN WAGER x3!           •   WIN WAGER x2!')
-    print('•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••')
+    print('•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••\n')
+    print('  MATCH TWO SYMBOLS:')
+    print('  €€- / ££- / $$- / ¥¥- or -€€ / -££ / -$$ / -¥¥')
+    print('  WIN WAGER x2!')
+    print()
+    print('  MATCH THREE SYMBOLS:')
+    print('  €€€ / £££ / $$$ / ¥¥¥')
+    print('  WIN WAGER x3!')
+    print('•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••\n')
     print()
     username = input("  What's your name? ")
     print()
-    print(f'                Hey {username}, you have {wallet} credits in your wallet!')
-    print('                          Minimum wager is 1 credit.')
+    print(f'  Hey {username}, you have {wallet} credits in your wallet!')
+    print('  Minimum wager is 1 credit.')
     print()
 
     while wallet > 0:
         try:
-            wager = int(input('                     How much would you like to wager..? '))
+            wager = int(input('  How much would you like to wager..? '))
         except:
-            print('                     Please wager a whole number of credits.\n')
+            print('  Please wager a whole number of credits.\n')
             continue
         if wager > wallet:
-            print(f'                    Sorry {username}, insufficient credits!\n')
+            print(f'  Sorry {username}, insufficient credits!\n')
             continue
         else:
             wallet -= wager
@@ -94,4 +107,3 @@ while True:
         print(f'                             You have {wallet} credits.')
 
     print(f"             Sorry {username}, you're broke! :( Thank you for playing.")
-
