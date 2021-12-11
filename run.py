d28e8de1a6c4529755ab1eeae76c0f8f2cd47090
alfa23 <paw235@gmail.com>
2021-12-11 10:16:53 +0000

    add: Complete base game coding, style & rework intro

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ variables for the game.
 """
 
 import random
-symbols = ['€', '£', '$']
+symbols = ['€', '£', '$', '¥']
 # taxman = ['@']
 
 # """
@@ -14,70 +14,84 @@ symbols = ['€', '£', '$']
 # Output user info and instructions
 # """
 
-# while True:
+while True:
 
-wallet = 100
+    wallet = 100
 
-print('The Python ONE ARM BANDIT\n')
-print()
-print('(Orig. US,gambling) A gaming machine\n')
-print('having a long arm-like handle at one\n')
-print('side that a player pulls down to make\n')
-print('reels spin; the player wins money or\n')
-print('tokens when certain combinations of\n')
-print('symbols line up on these reels.\n')
-print()
-print('AKA: Fruit machine, slot machine\n')
-print()
-print('• https://en.wiktionary.org/wiki/\n')
-print('  one-armed_bandit\n')
-print()
-print('****************************************\n')
-print('MATCH THREE SYMBOLS:\n')
-print('£££ / $$$ / €€€\n')
-print('WIN wager x 3!!!\n')
-print()
-print('MATCH TWO SYMBOLS:\n')
-print('££- / $$- / €€- and -££ / -$$ / -€€\n')
-print('WIN wager x 2!!\n')
-print()
-username = input('What is your name? ')
-print()
-print(f'Hey {username}, you have {wallet} credits in your wallet!\n')
-print('Minimum wager is 1 credit.\n')
-
-while wallet > 0:
-    try:
-        wager = int(input('How much do you want to wager? \n'))
-    except:
-        print('Please wager a whole number of credits.\n')
-        continue
-    if wager > wallet:
-        print(f'Sorry {username}, insufficient credits!\n')
-        continue
-    else:
-        wallet -= wager
-        reel_1 = random.choice(symbols)
-        reel_2 = random.choice(symbols)
-        reel_3 = random.choice(symbols)
-    
-    print(f'     |{random.choice(symbols)}|{random.choice(symbols)}|{random.choice(symbols)}|')
-    print(f' WIN•|{reel_1}|{reel_2}|{reel_3}|•LINE')
-    print(f'     |{random.choice(symbols)}|{random.choice(symbols)}|{random.choice(symbols)}|\n')
-
-    if reel_1 == reel_2 and reel_2 == reel_3:
-        winnings = wager * 3
-        print(f'Awesome, you matched three and won {winnings} credits!\n')
-        wallet += winnings
-    elif reel_1 == reel_2 or reel_2 == reel_3:
-        winnings = wager * 2
-        print(f'Not bad, you matched two and won {winnings} credits!\n')
-        wallet += winnings
-    else:
-        print('Unlucky, you lost that spin.\n')
-    print('********************')
     print()
-    print(f'You have {wallet} credits.\n')
+    print('############################• T H E   P Y T H O N •############################')
+    print('#                                                                             #')
+    print('#   #     ##   ###   #   #  ####  ###     ###    ##   #  #  ###   ###  #####  #')
+    print('#  ##    #  #  #  #  ## ##  #     #  #    #  #  #  #  ## #  #  #   #     #    #')
+    print('#   # ## ####  ###   # # #  ###   #  #    ###   ####  # ##  #  #   #     #    #')
+    print('#   #    #  #  #  #  #   #  #     #  #    #  #  #  #  #  #  #  #   #     #    #')
+    print('#  ###   #  #  #  #  #   #  ####  ###     ###   #  #  #  #  ###   ###    #    #')
+    print('#                                                                             #')
+    print('###############################################################################')
+    print()
+    print('  https://en.wiktionary.org/wiki/one-armed_bandit:')
+    print()
+    print('  From one-armed (“having only one arm”) + bandit (“one who robs others in')
+    print('  a lawless area, especially as part of a group; one who cheats others”),')
+    print('  referring to the fact that the machine is operated by a single handle')
+    print('  and “steals” money from losing players.')
+    print()
+    print('  (Orig. US, gambling) A gaming machine having a long arm-like handle at one')
+    print('  side that a player pulls down to make reels spin; the player wins money or')
+    print('  tokens when certain combinations of symbols line up on these reels.')
+    print()
+    print('  AKA: Fruit machine, poker machine/pokie/pokie machine, slot machine')
+    print()
+    print('•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••')
+    print('  MATCH THREE SYMBOLS:    •   MATCH TWO SYMBOLS:')
+    print('  €€€ / £££ / $$$ / ¥¥¥   •   €€- / ££- / $$- / ¥¥- or -€€ / -££ / -$$ / -¥¥')
+    print('  WIN WAGER x3!           •   WIN WAGER x2!')
+    print('•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••')
+    print()
+    username = input("  What's your name? ")
+    print()
+    print(f'                Hey {username}, you have {wallet} credits in your wallet!')
+    print('                          Minimum wager is 1 credit.')
+    print()
 
-print(f"Sorry {username}, you're broke :(\n")
-print('Thank you for playing\n')
+    while wallet > 0:
+        try:
+            wager = int(input('                     How much would you like to wager..? '))
+        except:
+            print('                     Please wager a whole number of credits.\n')
+            continue
+        if wager > wallet:
+            print(f'                    Sorry {username}, insufficient credits!\n')
+            continue
+        else:
+            wallet -= wager
+            reel_1 = random.choice(symbols)
+            reel_2 = random.choice(symbols)
+            reel_3 = random.choice(symbols)
+        
+        print()
+        print('                        ••••••••••••••••••••••••••••••')
+        print(f'                       •        | {random.choice(symbols)} | {random.choice(symbols)} | {random.choice(symbols)} |         •')
+        print('                      •         —————————————          •')
+        print(f'                     •    WIN • | {reel_1} | {reel_2} | {reel_3} | • LINE    •')
+        print('                      •         —————————————          •')
+        print(f'                       •        | {random.choice(symbols)} | {random.choice(symbols)} | {random.choice(symbols)} |         •')
+        print('                        ••••••••••••••••••••••••••••••')
+        print()
+
+
+        if reel_1 == reel_2 and reel_2 == reel_3:
+            winnings = wager * 3
+            print(f'                Awesome, you matched three and won {winnings} credits!')
+            wallet += winnings
+        elif reel_1 == reel_2 or reel_2 == reel_3:
+            winnings = wager * 2
+            print(f'                 Not bad, you matched two and won {winnings} credits!')
+            wallet += winnings
+        else:
+            print('                         Unlucky, you lost that spin.')
+        print()
+        print(f'                             You have {wallet} credits.')
+
+    print(f"             Sorry {username}, you're broke! :( Thank you for playing.")
+
